49598d809b5aee163c0e8f166d504976c63563ca
eastw00d <33877108+eastw00d@users.noreply.github.com>
2017-11-22 16:41:58 -0700

    home/index template

---
+++ sc/application/views/home.php
@@ -0,0 +1,19 @@
+<?php
+	defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!-- Intro Desktop Section -->
+<section id="intro" class="dark-bg">
+ 	<div class="bg-img overlay-dark80 dark-bg img-responsive2 slide-bg-image parallax parallax-section1">
+        <div class="js-fullscreen-height container">
+            <div class="intro-content">
+                <div class="intro-content-inner">
+               	<div align="center">
+					<a href="/home/landing" class="btn btn-lg btn-lblue pulse">FIND OUT TODAY</a>
+				</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Intro Desktop Section -->
