1195e710c5ace8ef9e7fbb050dff0ebd2b4e12c6
eastw00d <33877108+eastw00d@users.noreply.github.com>
2017-11-22 16:48:55 -0700

    thanks page that is invoked after lead is created

---
+++ sc/application/views/thanks.php
@@ -0,0 +1,17 @@
+<section id="intro" class="intro">
+    <div class="bg-image parallax parallax-background2">
+        <div class="full-screen-intro container" style="min-height: 400px;">
+            <div class="container text-center">
+                <div class="hidden-xs intro-content-inner" >
+                    <h1 class="large mb-25">Thank you, we will contact you</h1>
+                    <br />
+                </div>
+				<div class="visible-xs intro-content-inner" >
+                    <h1 class="mb-25">Thank you, we will contact you</h1>
+                    <br />
+                </div>
+            </div>
+        </div>
+    </div>
+	<?php session_unset(); ?>
+</section>
