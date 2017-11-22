5ecd1675db7217da0e92b5f61462e4e4c3543d1b
eastw00d <33877108+eastw00d@users.noreply.github.com>
2017-11-22 16:58:00 -0700

    DB connection, config updates and settign to autoload  database library

--- sc/application/config/autoload.php
+++ sc/application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = ['form_validation', 'template'];
+$autoload['libraries'] = ['form_validation', 'template', 'database'];
 
 /*
 | -------------------------------------------------------------------
