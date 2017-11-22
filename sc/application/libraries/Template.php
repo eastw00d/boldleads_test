079de0b3d815cf16638528a853ae8ca0054efb4b
eastw00d <33877108+eastw00d@users.noreply.github.com>
2017-11-22 16:52:35 -0700

    added comments

--- sc/application/libraries/Template.php
+++ sc/application/libraries/Template.php
@@ -4,18 +4,24 @@
  * @author		Alex
  */
 
-// Little class to operate custom templates/views
+// class to operate custom templates/views
 
 class Template {
     private $ci;
     private $template_name;
-    
+  	// constructor that instantiates CodeIgniter instance  
     public function __construct () {
         $this->ci =& get_instance();
     }
 
+	/**
+ 	* Function load  is meant to load  templates and attach css and js if needed to the specific template
+	*
+ 	*/
+	
     public function load($tpl_view, $body_view = null, $data = null) {
         $this->template_name = $tpl_view;
+		// building data array to pass into view
         $data['body'] = '';
 
         if (!is_null($body_view)) {
@@ -25,13 +31,18 @@ class Template {
                 $data['body'] = $this->ci->load->view($body_view, $data, TRUE);
             }
         }
-
+		// here we are attaching js and css
         $data['css'] = $this->css();
         $data['js'] = $this->js();
+		// let CodeIgniter to load template
         $this->ci->load->view('templates/layout', $data);
     }
 
-    private function css () {
+	/**
+ 	* private Function css is meant to assit load function to attach css to the specific template
+	*
+ 	*/
+    private function css() {
         $file_spec = APPPATH . "views/templates/css_{$this->template_name}.php";
         $result = '';
 
@@ -45,7 +56,11 @@ class Template {
         return $result;
     }
 
-    private function js () {
+	/**
+ 	* private Function js is meant to assit load function to attach js to the specific template
+	*
+ 	*/
+    private function js() {
         $file_spec = APPPATH . "views/templates/js_{$this->template_name}.php";
         $result = '';
 
